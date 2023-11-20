edf160319e41fee4cea1431c7b91f16cb6713d38
ILIA GOLUBEV <info@buromir.com>
2023-11-20 20:57:51 +0300

    Lesson 12 Task Pro

---
+++ lesson_12_home_pro/django_chatbot/chatbot/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# render start.html page
+def start(request):
+    return render(request, "start.html")
+
+# render chatbot.html page
+def chatbot(request):
+    return render(request, "chatbot.html")
+
+def stat(request):
+    return render(request, "stat.html")
